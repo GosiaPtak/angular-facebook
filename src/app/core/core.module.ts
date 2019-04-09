429e289ad746176a135947c4b176176e35cd6f90
GosiaPtak <ptakma@gmail.com>
2019-04-09 11:47:24 +0200

    refactor: changed from app to core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreRoutingModule } from './core-routing.module';
-import { CoreComponent } from './core.component';
 import { AppComponent } from './components/app/app.component';
 
 @NgModule({
   declarations: [
-    CoreComponent,
     AppComponent
   ],
   imports: [
@@ -15,6 +13,6 @@ import { AppComponent } from './components/app/app.component';
     CoreRoutingModule
   ],
   providers: [],
-  bootstrap: [CoreComponent]
+  bootstrap: [AppComponent]
 })
 export class CoreModule { }
