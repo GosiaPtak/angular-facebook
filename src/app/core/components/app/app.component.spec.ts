29ac7f68718ffaa1a46151f4addb0ad5534b0f6d
GosiaPtak <ptakma@gmail.com>
2019-04-09 12:15:40 +0200

    feat(feature): teardown tests

--- src/app/core/components/app/app.component.spec.ts
+++ src/app/core/components/app/app.component.spec.ts
@@ -3,23 +3,29 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
+
+  let fixture = null;
+  let component = null;
+  let $component = null;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AppComponent ]
     })
     .compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
+    $component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
+  afterEach(() => {
+    $component.remove();
+});
+  it('should create the app', () => {
     expect(component).toBeTruthy();
   });
 });
