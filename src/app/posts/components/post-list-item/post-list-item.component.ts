aae20ab4101c84d5d95e193e2ce7e7e4e316cffb
GosiaPtak <ptakma@gmail.com>
2019-04-09 15:59:52 +0200

    feat(post): enable onpush srategy in change Detector

--- src/app/posts/components/post-list-item/post-list-item.component.ts
+++ src/app/posts/components/post-list-item/post-list-item.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { Post } from '../../interfaces/post.interface';
 
 @Component({
   selector: 'app-post-list-item',
   templateUrl: './post-list-item.component.html',
-  styleUrls: ['./post-list-item.component.scss']
+  styleUrls: ['./post-list-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush //tylko wtedy kiedy wprowadzane sa dane
 })
 export class PostListItemComponent implements OnInit {
   @Input() post: Post = null;
